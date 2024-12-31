549811063006766f1a140a2415877ebc003d2fac
Nirithle <69992217+Nirithle@users.noreply.github.com>
2024-12-31 14:49:36 +0100

    lab_04

--- Katalog_glowny_projektu /Folder_projektu/myapp/models.py
+++ Katalog_glowny_projektu /Folder_projektu/myapp/models.py
@@ -5,6 +5,8 @@ from django.db import models
 # deklaracja statycznej listy wyboru do wykorzystania w klasie modelu
 MONTHS = models.IntegerChoices('Miesiace', 'Styczeń Luty Marzec Kwiecień Maj Czerwiec Lipiec Sierpień Wrzesień Październik Listopad Grudzień')
 
+PLCIE = models.IntegerChoices('PLEC', 'Kobieta Mężczyzna Inna')
+
 SHIRT_SIZES = (
         ('S', 'Small'),
         ('M', 'Medium'),
@@ -39,11 +41,13 @@ class Osoba(models.Model):
     )
     imie = models.CharField(max_length=40, blank = False, null = False)
     nazwisko = models.CharField(max_length=60, blank = False, null = False)
-    plec = models.CharField(max_length=1, choices = PLEC_CHOICES, default = PLEC_CHOICES[2][0])
+    plec = models.IntegerField(choices = PLCIE.choices, default = PLCIE.choices[2][0])
     stanowisko = models.ForeignKey("Stanowisko", on_delete = models.CASCADE)
+    data_dodania = models.DateField(auto_now_add = True, editable = False)
+
 
     def __str__(self):
-        return f'(self.imie){self.nazwisko}'
+        return f'{self.imie} {self.nazwisko}'
         #return str(self.imie) + " " + str(self.nazwisko)
 
 class Stanowisko(models.Model):
@@ -51,4 +55,4 @@ class Stanowisko(models.Model):
     opis = models.TextField(blank = False, null = False)
 
     def __str__(self):
-        return self.name
+        return self.nazwa
